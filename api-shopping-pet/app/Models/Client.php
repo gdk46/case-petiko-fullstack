65d17131aa065a9bfd455e32aa223959938f326c
gdk46 <gleissonneves2017@gmail.com>
2021-06-14 19:22:14 -0300

    feature: add feature Create client and route post

--- api-shopping-pet/app/Models/Client.php
+++ api-shopping-pet/app/Models/Client.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    protected $table = "clients";
+    protected $primaryKey = "id";
+    protected $guarded = [];
 }
